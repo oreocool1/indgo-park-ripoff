5c08c5a9083cffb1d590b02470d907f2adc11c40
Oliver Howe <21howeo@utcsheffield.org.uk>
2024-06-05 13:28:48 +0100

    rambally introduction

--- indigo rip off.py
+++ indigo rip off.py
@@ -11,7 +11,8 @@ while end != "Yes" and end != "Death1" and end != "Death2":
         print("You enter the room")
         print('''you look around and see a TV screean
         You walk towards the TV... 
-        it sodenly actavates''')
+        it sodenly actavates and you see a racoon on the screen''')
+        print("Hi im rambally. Rambally the Racoon and welcome to indio park. to get registerd go to the regstration desk on your left")
     elif option == "[B]":
         Death = "bolders"
         end = "Death1"
