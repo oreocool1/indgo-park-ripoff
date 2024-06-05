1861d53a5be7ebbc4d43a127ca2d62ae6082a697
Oliver Howe <21howeo@utcsheffield.org.uk>
2024-06-05 12:38:54 +0100

    start

--- indigo rip off.py
+++ indigo rip off.py
@@ -3,6 +3,7 @@ import webbrowser
 end = "no"
 Death = "alive"
 while end != "Yes" or end != "Death1" or end != "Death2":
-    print ("")
+    print("You were urban exploring before you found and an abandond park anddeided to enter it")
+    option = input (''' ''')
 if end == "Death1":
     print ("You died to" , Death , "Close and reopen the game to try again")
