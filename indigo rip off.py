18ce7f4eae839f3d2472c3ba75bac42fe25b9962
Oliver Howe <21howeo@utcsheffield.org.uk>
2024-06-05 12:33:13 +0100

    Add project files.

---
+++ indigo rip off.py
@@ -0,0 +1,8 @@
+import time
+import webbrowser
+end = "no"
+Death = "alive"
+while end != "Yes" or end != "Death1" or end != "Death2":
+    print ("")
+if end == "Death1":
+    print ("You died to" , Death , "Close and reopen the game to try again")
