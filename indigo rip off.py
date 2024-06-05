81d85019f54ad32ed3d2f48c11359dbfdf49b8d0
Oliver Howe <21howeo@utcsheffield.org.uk>
2024-06-05 13:26:13 +0100

    majic

--- indigo rip off.py
+++ indigo rip off.py
@@ -2,8 +2,18 @@ import time
 import webbrowser
 end = "no"
 Death = "alive"
-while end != "Yes" or end != "Death1" or end != "Death2":
+while end != "Yes" and end != "Death1" and end != "Death2":
     print("You were urban exploring before you found and an abandond park anddeided to enter it")
-    option = input (''' ''')
+    option = input (''' You see an open door What do you do 
+    Enter the doorway [A]
+    Try move the rubble[B]''')
+    if option == "[A]":
+        print("You enter the room")
+        print('''you look around and see a TV screean
+        You walk towards the TV... 
+        it sodenly actavates''')
+    elif option == "[B]":
+        Death = "bolders"
+        end = "Death1"
 if end == "Death1":
     print ("You died to" , Death , "Close and reopen the game to try again")
