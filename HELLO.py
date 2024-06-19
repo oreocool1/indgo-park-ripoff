578ebdc43c9c142dfe29b470d94055bfb6341c0e
oreocool1 <oliverhowe2018@gmail.com>
2024-06-19 11:39:43 +0100

    new project

---
+++ HELLO.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+def Welcome():
+    print ("Hi welcome to The Forrest")
+    print("To play you will be given options to chose and try to excape")
+    print("First we need some data")
+    Name = input ("What is your Name?")
+    Pronowns = input ("What pronowns do you use")
