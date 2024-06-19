a5886f2720a17c64f6c37fb69a7bef90e1bc7a05
oreocool1 <oliverhowe2018@gmail.com>
2024-06-19 11:57:36 +0100

    pronowns file

--- HELLO.py
+++ HELLO.py
@@ -1,9 +1,17 @@
+from os import name
+from sys import _enablelegacywindowsfsencoding
 import time
 import webbrowser
 
 def Welcome():
-    print ("Hi welcome to The Forrest")
-    print("To play you will be given options to chose and try to excape")
-    print("First we need some data")
-    Name = input ("What is your Name?")
-    Pronowns = input ("What pronowns do you use")
+    name = open("test.txt", "r")
+    print(name.read())
+    hi = input("These are your current pronowns do you want to change them?")
+    if hi == "yes" or hi == "Yes" or hi == "YES":
+        name = open("test.txt","w")
+        pronowns = input("what are your new pronowns?")
+        name.write(pronowns)
+    elif hi == "no" or hi == "No" or hi == "NO":
+        name.close()
+    else: print ("invalid input")
+Welcome()
