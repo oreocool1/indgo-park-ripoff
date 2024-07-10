6f79d5c11226b32ac912f74f05a538a1e27d556a
oreocool1 <oliverhowe2018@gmail.com>
2024-07-10 13:36:40 +0100

    MAJOR changes

--- indigo park unoffical.py
+++ indigo park unoffical.py
@@ -21,12 +21,11 @@ def menu():
 New Game [N]
 Continue [C]''')
     if opton == ("N"):
-        Maingame()
+        Start()
     elif opton == ("C"):
-        Playerdata = open("DATA.txt", "r")
-        
-def Maingame():
-    global end, Death, ramplush, mollyplush, player_name
+        Playerdata = open("DATA.txt", "r")  
+def Start():
+    global end, Death
     
     while end != "Yes" and end != "Death1" and end != "Death2":
         print("You were urban exploring before you found an abandoned park and decided to enter it")
@@ -35,100 +34,103 @@ Enter the doorway [A]
 Try to move the rubble [B]
 ''')
 
-        if option == "A":
-            while end != "Yes" and end != "Death1" and end != "Death2":
-                print("You enter the room")
-                print('''You look around and see a TV screen
+    if option == "A":
+        entry()
+    elif option == "B":
+            Death = "boulders"
+            end = "Death1"
+    else:
+        print("Please choose A or B DONT TYPE IT IN SQUARE BRACKETS!. (please wait for the next text before typing)")
+def entry():
+    global end, Death, ramplush, mollyplush
+    while end != "Yes" and end != "Death1" and end != "Death2":
+        print("You enter the room")
+        print('''You look around and see a TV screen
 You walk towards the TV...
 It suddenly activates and you see a raccoon on the screen''')
-                print("Hi, I'm Rambally. Rambally the Raccoon and welcome to Indigo Park. To get registered, go to the registration desk on your left")
+        print("Hi, I'm Rambally. Rambally the Raccoon and welcome to Indigo Park. To get registered, go to the registration desk on your left")
                 
-                option = input('''What do you do?
+        option = input('''What do you do?
 Go to the desk on your left [A]
 Brainlessly walk left [B]
 ''')
                 
-                if option == "A":
-                    print("Let me just get a good look at you... I don't recognize you, please insert some details ~Rambally")
-                    time.sleep(2.0)
-                    player_name = input("What is your name? ")
-                    print(f"Hi {player_name}, meet me at the main gate and I'll let you in")
-                    time.sleep(1.0)
+        if option == "A":
+            reg()
+        elif option == "B":
+            print("Buddy, you're going the wrong way. The registration desk is this way (points to the left)")
+            time.sleep(0.9)
+        else:
+            print("Please choose [A] or [B] and type it in the square brackets. (please wait for the next text before typing)")
+def reg():
+    global end, Death, player_name
+    print("Let me just get a good look at you... I don't recognize you, please insert some details ~Rambally")
+    time.sleep(2.0)
+    player_name = input("What is your name? ")
+    print(f"Hi {player_name}, meet me at the main gate and I'll let you in")
+    time.sleep(1.0)
                     
-                    while end != "Yes" and end != "Death1" and end != "Death2":
-                        print("What's the hold-up? The gate should be open... my mistake, the gate is not open. Seems like an error, boot up the generator to get the gate up")
-                        time.sleep(2.0)
-                        option = input('''Should you turn on the generator?
+    while end != "Yes" and end != "Death1" and end != "Death2":
+        print("What's the hold-up? The gate should be open... my mistake, the gate is not open. Seems like an error, boot up the generator to get the gate up")
+        time.sleep(2.0)
+        option = input('''Should you turn on the generator?
 No [A]
 Yes [B]
 ''')
 
-                        if option == "A":
-                            print("... Nothing happened")
-                            time.sleep(1.0)
-                        elif option == "B":
-                            print("You turn on the generator and go back to the gate.")
-                            time.sleep(1.0)
-                            print("Alright, for real this time. Welcome to Indigo Park.")
-                            time.sleep(0.5)
+    if option == "A":
+        print("... Nothing happened")
+        time.sleep(1.0)
+    elif option == "B":
+        print("You turn on the generator and go back to the gate.")
+        time.sleep(1.0)
+        print("Alright, for real this time. Welcome to Indigo Park.")
+        time.sleep(0.5)
+    else:
+        print("Please choose an option")
                             
-                            while end != "Yes" and end != "Death1" and end != "Death2":
-                                option = input('''What do you want to do?
+    while end != "Yes" and end != "Death1" and end != "Death2":
+        option = input('''What do you want to do?
 Inspect Rambally plushy [A]
 Inspect mysterious plushy [B]
 Walk into the park [C]
 ''')
                                 
-                                if option == "A" and not ramplush:
-                                    print("Well done, you have collected the Rambally plushy")
-                                    ramplush = True
-                                    time.sleep(1.0)
-                                elif option == "A" and ramplush:
-                                    print("You have already collected this plush")
-                                    time.sleep(1.0)
-                                elif option == "B" and not mollyplush:
-                                    print("Well done, you have collected the mysterious plushy")
-                                    mollyplush = True
-                                    time.sleep(1.0)
-                                elif option == "B" and mollyplush:
-                                    print("You have already collected this plush.")
-                                    time.sleep(1.0)
-                                elif option == "C":
-                                    end = "Yes"
-                                    print("You enter the park")
-                                    time.sleep(1.0)
-                                    
-                                    while end != "Yes" and end != "Death1" and end != "Death2":
-                                        print("The entrance collapses leaving you trapped inside the entrance")
-                                        option = input('''Where do you go?
+    if option == "A" and not ramplush:
+        print("Well done, you have collected the Rambally plushy")
+        ramplush = True
+        time.sleep(1.0)
+    elif option == "A" and ramplush:
+        print("You have already collected this plush")
+        time.sleep(1.0)
+    elif option == "B" and not mollyplush:
+        print("Well done, you have collected the mysterious plushy")
+        mollyplush = True
+        time.sleep(1.0)
+    elif option == "B" and mollyplush:
+        print("You have already collected this plush.")
+        time.sleep(1.0)
+    elif option == "C":
+        end = "Yes"
+        print("You enter the park")
+        time.sleep(1.0)
+    else:
+        print("Unable to do this actiom")
+def ramramrail():
+    while end != "Yes" and end != "Death1" and end != "Death2":
+        print("The entrance collapses leaving you trapped inside the entrance")
+        option = input('''Where do you go?
 Into Rambally railway? [A]
 Try to move the rubble [B]
 ''')
-                                        
-                                        if option == "A":
-                                            webbrowser.open_new("https://youtu.be/kaZxq9I0zUQ?si=6on3TxXcsp8MrSRq")
-                                            end = "Yes"
-                                        if option == "B":
-                                            Death = "crushed by boulders"
-                                            end = "Death2"
-                                else:
-                                    print("Unable to do this action")
-                        else:
-                            print("Please choose an option")
-                        time.sleep(0.5)
-                elif option == "B":
-                    print("Buddy, you're going the wrong way. The registration desk is this way (points to the left)")
-                    time.sleep(0.9)
-                else:
-                    print("Please choose [A] or [B] and type it in the square brackets. (please wait for the next text before typing)")
-        elif option == "B":
-            Death = "boulders"
-            end = "Death1"
-        else:
-            print("Please choose A or B DONT TYPE IT IN SQUARE BRACKETS!. (please wait for the next text before typing)")
-        time.sleep(1.0)
-
-Maingame()
+        if option == "A":
+            webbrowser.open_new("https://youtu.be/kaZxq9I0zUQ?si=6on3TxXcsp8MrSRq")
+            end = "Yes"
+        if option == "B":
+            Death = "crushed by boulders"
+            end = "Death2"
+    else:
+        print("Chose an option")
 
 if end == "Death1":
     print("You died to", Death, "Close and reopen the game to try again")
