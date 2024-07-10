9e5e40e4c3263521f2efcf4be7963c7962480943
oreocool1 <oliverhowe2018@gmail.com>
2024-07-10 11:49:52 +0100

    Bye Bye :3 bug fixes

--- NOTmain game.py
+++
@@ -1,4 +0,0 @@
-if end == "Death1":
-    print ("You died to" , Death , "Close and reopen the game to try again")
-if end == "Death2":
-    print("Hey buddie wake up please.~Rambally. You died to",Death,"close and reopen to try again")
